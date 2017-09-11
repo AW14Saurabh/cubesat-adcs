5a525eb15c49ca768894253cc160c47e3ecde516
Mark Yeo <mark.yeo@student.unsw.edu.au>
2017-09-12 00:40:18 +1000

    Finished program minus PID constant tuning

--- Reaction Wheel Platform/RWS v2/detumblev2/detumblev2.ino
+++ Reaction Wheel Platform/RWS v2/detumblev2/detumblev2.ino
@@ -1,19 +1,20 @@
-//Program to detumble Reaction Wheel System v2
+//Program to detumble/point Reaction Wheel System v2 using Control Box
 //Mark Yeo; mark.yeo@student.unsw.edu.au
-//Last Modified 11Sept'17
+//Last Modified 2017-09-12
 
+//Libraries
 #include <Wire.h>
 #include <FaBo9Axis_MPU9250.h>
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <PID_v1.h>
-
+//Pins
 #define RED_LED 8
 #define GRN_LED 9
 #define MTR_VR 5
 #define MTR_ZF 7
-
+//States
 #define DISABLED 0
 #define ENABLED 1
 #define DETUMBLE 0
@@ -21,38 +22,110 @@
 #define CCW 0
 #define CW 1
 
+/*
+//Function Definitions
+void setupIO();
+void setupMPU9250();
+void calibGyro();
+void setupRadio();
+void setupPID();
+bool getPacket(struct BoxCmd *box);
+float getWz();
+int dAngle(float wz);
+void driveMotor(int mtrVel);
+*/
+//Just a bunch of structs
 struct Bias{
   float x;
   float y;
   float z;
 };
-
 struct Packet{
   char h;
   int d;
 };
-
 struct BoxCmd{
   bool mtrEna;
   bool mode;
   int angle;
 };
 
-
 //Setup + Global Variables
 FaBo9Axis fabo_9axis;
 RF24 radio(19,18);
 const byte rxAddr[6] = "00001";
+double g_PIDSetpoint, g_PIDInput, g_PIDOutput;  //PID variables
+double g_dKp=2, g_dKi=0, g_dKd=0;  //PID tuning constants for detumbling
+double g_pKp=2, g_pKi=0, g_pKd=0;  //PID tuning constants for pointing
+PID PIDCtrl(&g_PIDInput, &g_PIDOutput, &g_PIDSetpoint, g_dKp, g_dKi, g_dKd, DIRECT);
 struct Bias g_gyroBias;
-int g_prevPollTime = 0;
+unsigned long g_prevPollTime = 0;
+unsigned long g_prevCmdTime = 0;
 int g_currAngle = 0;
-int g_prevCmdTime = 0;
+int g_cycleTime = 100;
+
+
+
+void setup(){
+  Serial.begin(9600);
+  setupIO();
+  digitalWrite(RED_LED, HIGH);
+  setupMPU9250();
+  calibGyro();
+  setupRadio();
+  setupPID();
+  digitalWrite(RED_LED, LOW);
+  g_prevPollTime = millis();
+  g_prevCmdTime = millis();
+}
+
+
+
+void loop(){
+  //=== Get data from gyro ===
+  float wz = getWz(); //Poll gyro
+  g_currAngle += dAngle(wz);  //Calculate current angle using basic integration
+
+  //=== Get commands from controller box ===
+  struct BoxCmd box;
+  bool boxConnected = true;
+  if (getPacket(&box)){
+    digitalWrite(GRN_LED, HIGH);
+    g_prevCmdTime = millis();
+  } else {
+    if (millis() - g_prevCmdTime > 2000){ //if 2s since last message received from box
+      digitalWrite(GRN_LED, LOW);
+      boxConnected = false;
+      Serial.println("Lost connection to box, disabling motor");
+    }
+  }
+
+  //=== Calculate + output speed to motor ===
+  int mtrVel = 0;
+  if (boxConnected && box.mtrEna == ENABLED){
+    if (box.mode == DETUMBLE){
+      g_PIDInput = wz;
+      g_PIDSetpoint = 0;
+      PIDCtrl.SetTunings(g_dKp, g_dKi, g_dKd);
+    } else {// i.e. box.mode == POINT
+      g_PIDInput = g_currAngle;
+      g_PIDSetpoint = box.angle;
+      PIDCtrl.SetTunings(g_pKp, g_pKi, g_pKd);
+    }
+    PIDCtrl.Compute();
+    mtrVel = g_PIDOutput;
+  }
+  driveMotor(mtrVel);
+  
+  delay(g_cycleTime);
+}
 
 
 
 
 
 
+//=== Setup Functions ===
 void setupIO(){
   pinMode(RED_LED, OUTPUT);
   pinMode(GRN_LED, OUTPUT);
@@ -104,31 +177,16 @@ void setupRadio(){
   radio.startListening();
 }
 
-void setup(){
-  Serial.begin(9600);
-  setupIO();
-  digitalWrite(RED_LED, HIGH);
-  setupMPU9250();
-  calibGyro();
-  setupRadio();
-  digitalWrite(RED_LED, LOW);
-  g_prevPollTime = millis();
-  g_prevCmdTime = millis();
+void setupPID(){
+  g_PIDInput = 0;
+  g_PIDSetpoint = 0;
+  PIDCtrl.SetMode(AUTOMATIC); //turn the PID on
+  PIDCtrl.SetOutputLimits(-255, 255); //clamps the output to -255 to 255
+  PIDCtrl.SetSampleTime(g_cycleTime);
+  PIDCtrl.SetControllerDirection(REVERSE);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//=== Loop Functions ===
 bool getPacket(struct BoxCmd *box){
   bool rxFlag = false;
   while (radio.available()){
@@ -164,7 +222,7 @@ float getWz(){
 }
 
 int dAngle(float wz){
-  int currPollTime = millis();
+  unsigned long currPollTime = millis();
   int changeInAngle = int(wz*(currPollTime-g_prevPollTime)/1000.0); //does weird stuff if RHS is left as a float - probably a result of some weird float precision stuff
   g_prevPollTime = currPollTime;
   return changeInAngle;
@@ -194,59 +252,4 @@ void driveMotor(int mtrVel){
   analogWrite(MTR_VR, mtrSpd);
 }
 
-void loop(){
-  //Loop Parameters
-  int cycleTime = 100;
-  
-  
-  float wz = getWz(); //Poll gyro
-  g_currAngle += dAngle(wz);  //Calculate current angle using basic integration
-  
-  struct BoxCmd box;
-  bool boxConnected = true;
-  if (getPacket(&box)){
-    digitalWrite(GRN_LED, HIGH);
-    g_prevCmdTime = millis();
-  } else {
-    if (millis() - g_prevCmdTime > 3000){ //if 3s since last message received from box (doesn't like 2000 for some reason - turns off immediately)
-      digitalWrite(GRN_LED, LOW);
-      boxConnected = false;
-      Serial.println("Lost connection to box, disabling motor");
-    }
-  }
-  int mtrVel = 0;
-  if (boxConnected && box.mtrEna == ENABLED){
-    if (box.mode == DETUMBLE){
-      mtrVel = -30;
-    } else if (box.mode == POINT){
-      mtrVel = 60;
-    }
-  }
-  driveMotor(mtrVel);
-
-  
-  
-  
-  
-
-
-
-
-
-  //Serial.print(gz);
-  //Serial.print(",");
-  //Serial.println(g_currAngle);
-
-  //Serial.print("Angle: ");
-  //Serial.print(box.angle);
-  //Serial.print(" Mode: ");
-  //Serial.print(box.mode);
-  //Serial.print(" Enable: ");
-  //Serial.println(box.enable);
-
-  
-  
-  
-  delay(cycleTime);
-}
 
